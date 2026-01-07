9b7cc2eeec02ee8a566584746894a15589402566
dvss-dvss <kosevojdavid3@gmail.com>
2026-01-07 20:15:52 +0100

    Add user profile management: create Profile model, forms, and views; implement profile editing functionality and update templates

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,15 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import register
+from .views import register, edit
 
 urlpatterns = [
-     path('login/', auth_views.LoginView.as_view(), name='login'),
-     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-     path('password-change/', auth_views.PasswordChangeView.as_view(), 
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), 
          name='password_change'),
-     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
-     path('register/', register, name='register'),
+    path('register/', register, name='register'), 
+    path('edit/', edit, name='edit'), 
 ]
