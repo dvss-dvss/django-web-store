99e0d338ec28fe41890d0ea0ce6f637cc779a6c9
dvss-dvss <kosevojdavid3@gmail.com>
2026-01-28 18:53:13 +0100

    Add CSV export functionality for orders in admin; enhance OrderItem model with verbose names and update order creation form for authenticated users

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,35 @@
+import csv
+import datetime as dt
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import Order, OrderItem
 
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # запис першого рядку з інформацією заголовку
+    writer.writerow([field.verbose_name for field in fields])
+    # запис рядків даних
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, dt.datetime):
+                value = value.strftime("%d.%m.%Y")
+            data_row.append(value)
+        writer.writerow(data_row) 
+    return response
+export_to_csv.short_description = "Експорт в CSV"
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -10,6 +38,7 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    actions = [export_to_csv]
     list_display = [
         'id',
         'first_name',
